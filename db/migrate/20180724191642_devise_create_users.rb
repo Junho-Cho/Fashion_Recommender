3f0c618a12aa7b4ea288a3f6f047934d7160f84e
CHO JUN HO <silver2473@naver.com>
2018-07-26 13:23:35 +0000

    Main: Sign up & Sign in +

--- db/migrate/20180724191642_devise_create_users.rb
+++ db/migrate/20180724191642_devise_create_users.rb
@@ -4,8 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
+      t.string :nickname,           null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
+      ## Developer Data
+      t.integer :level,             default: 1
 
       ## Recoverable
       t.string   :reset_password_token
